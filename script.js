847b909e2a09b269240f315c40b7c199dc742c5d
aganicat <nagayev95@gmail.com>
2024-03-29 17:00:00 +0200

    2 task are done

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ console.log(firstName);
 console.log(firstName);
 
 // variable name conventions
-*/
 
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
@@ -32,3 +31,87 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+*/
+/*
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power 3 = 2 * 2 * 2
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+// Assignmanet operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 =100
+x++; // x = x + 1
+x--; // x = x -1
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <. >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+let xx, y;
+xx = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(xx, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+// Calculate BMIs
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+// Check if Mark has a higher BMI than John
+const markHigherBMI = BMIMark > BMIJohn;
+
+// Log the result to the console
+console.log(BMIJohn, BMIMark, markHigherBMI);
+*/
+/*
+const firstName = "Jonas";
+const job = " Teacher";
+const birthYear = 1991;
+const year = 2037;
+
+const jonas =
+  "I'm " + firstName + ", a " + (year - birthYear) + " years old" + job + "!";
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+console.log(
+  "String with \n\
+multiple \n\
+lines"
+);
+
+console.log(`String
+multiple
+lines`);
+*/
